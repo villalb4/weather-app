037f14e727aa45dc3948872bcbc014f51f73e909
villalb4 <villalb4angel@gmail.com>
2023-01-08 02:40:08 -0300

    add: dispatch getData and import Panel component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { getData } from './redux/thunks/getData';
+import { useDispatch } from 'react-redux';
+import Panel from './components/Panel/Panel';
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  dispatch(getData())
+
   return (
     <div className="App">
+      <Panel />
     </div>
   );
 }
