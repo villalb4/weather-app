79ed394f6465cd21a24d47add7f7b7d888834456
villalb4 <villalb4angel@gmail.com>
2023-01-08 20:50:59 -0300

    add: loading data into component

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Panel.css'
-// ------ icons ------ 
+import { useSelector } from 'react-redux'
+// ------ icons/images ------ 
 import my_location from '../../assets/icons/my_location.png'
+import pint_drop from '../../assets/icons/pin_drop.png'
+import cloud_background from '../../assets/representation/Cloud-background.png'
 
 function Panel() {
+
+  const weather = useSelector(e => e.weather.weather)
+
   return (
     <div className='Panel'>
       {/* ---- top panel ---- */}
@@ -18,20 +24,33 @@ function Panel() {
         </div>
       </div>
       {/* ---- mid panel ---- */}
-      <div className='Panel_mid'>
+      <div 
+        className='Panel_mid'
+      >
         <div className='Panel_divWeatherImage'></div>
         <div className='Panel_divTemp'>
-          <span className='Panel_Temp'></span>
+          <span className='Panel_temp'>{weather.temp}</span>
         </div>
         <div className='Panel_divWeatherDescription'>
-          <span className='Panel_weatherDescription'></span>
+          <span className='Panel_weatherDescription'>{weather.weather}</span>
         </div>
-        <div className='Panel_divBackgroundClouds'></div>
+        <div className='Panel_midBackground'>
+          <img src={cloud_background} alt="" />
+        </div>
       </div>
       {/* ---- bottom panel ---- */}
       <div className='Panel_bottom'>
-        <div className='Panel_'></div>
-        <div className='Panel_'></div>
+        <div className='Panel_divDate'>
+          <span className='Panel_today'>Today</span>
+          <span>•</span>
+          <span className='Panel_date'>{weather.toDay}</span>
+        </div>
+        <div className='Panel_divLocation'>
+          <div>
+            <img src={pint_drop} alt="" />
+          </div>
+          <span className='Panel_location'>{weather.name}</span>
+        </div>
       </div>
     </div>
   )
