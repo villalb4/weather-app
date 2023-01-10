dd40e4b86c1c0b05c4c06fa6e218833f68b26a3a
villalb4 <villalb4angel@gmail.com>
2023-01-10 20:15:20 -0300

    add: Weather component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { getData, getFiveDaysData } from './redux/thunks/getData';
 import { useDispatch } from 'react-redux';
 import Panel from './components/Panel/Panel';
+import Weather from './components/Weather/Weather';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <Panel />
+      <Weather />
     </div>
   );
 }
