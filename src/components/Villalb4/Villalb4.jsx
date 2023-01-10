7a022dda6c8ab93c4eaa99135241451edd301350
villalb4 <villalb4angel@gmail.com>
2023-01-10 20:17:21 -0300

    add: created and styled components

---
+++ src/components/Villalb4/Villalb4.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Villalb4.css'
+
+function Villalb4() {
+  return (
+    <div className='Villalb4'>
+      <p className='Villalb4_p'>created by <a className='Villalb4_a' href='https://github.com/villalb4'>villalb4</a> - devChallenges.io</p>
+    </div>
+  )
+}
+
+export default Villalb4
