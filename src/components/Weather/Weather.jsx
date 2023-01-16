d36808124a3cce4ee9414e816249b1af52109d52
villalb4 <villalb4angel@gmail.com>
2023-01-16 16:28:01 -0300

    add: dispatch changeDegree

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,16 +1,32 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './Weather.css'
 import Villalb4 from './Villalb4/Villalb4'
 import DayInfo from './DayInfo/DayInfo'
 import Forecast from './Forecast/Forecast'
+import { changeDegree } from '../../redux/slices/weather'
 
 function Weather() {
+  
+  const dispatch = useDispatch()
+
+  const degrees = useSelector(e => e.weather.degree)
+
+  const handleC = () => {
+    dispatch(changeDegree("°C"))
+  }
+
+  const handleF = () => {
+    dispatch(changeDegree("°F"))
+  }
+
   return (
     <div className='Weather'>
       <div className='Weather_top'>
-        <div className='Weather_divC'>
+        <div className={degrees === "°C" ? 'Weather_divC active' : 'Weather_divC'} onClick={handleC}>
           <span className='Weather_c'>°C</span>
         </div>
-        <div className='Weather_divF'>
+        <div className={degrees === "°F" ? 'Weather_divF active' : 'Weather_divF'} onClick={handleF}>
           <span className='Weather_f'>°F</span>
         </div>
       </div>
